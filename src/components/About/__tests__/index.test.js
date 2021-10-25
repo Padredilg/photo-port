0ca690de28c8ba40bd717fe48cad9ae01e6cafee
Padredilg <padredilg@knights.ucf.edu>
2021-10-25 18:26:40 -0400

    created tests for Nav component

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -8,13 +8,11 @@ afterEach(cleanup);
 describe('About component', () => {
     // First Test
     it('renders', () => {
-        //does not actually render, just tests the capability to render
         render(<About />);
     });
 
     // Second Test
     it('matches snapshot DOM node structure', () => {
-        // actually renders the about instead of just testing whether it can render
         const { asFragment } = render(<About />);
         expect(asFragment()).toMatchSnapshot();
     })
