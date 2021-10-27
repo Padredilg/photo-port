86543267ec4ba404976e8796633388ea8c509ec4
Padredilg <padredilg@knights.ucf.edu>
2021-10-27 10:41:51 -0400

    added contact component

---
+++ src/components/Contact/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function ContactForm() {
+    return (
+        <section>
+            <h1>Contact me</h1>
+            <form id="contact-form">
+                {/* Name */}
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" name="name" />
+                </div>
+                {/* Email */}
+                <div>
+                    <label htmlFor="email">Email address:</label>
+                    <input type="email" name="email" />
+                </div>
+                {/* Message */}
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <textarea name="message" rows="5"  />
+                </div>
+                {/* submit */}
+                <button type="submit">Submit</button>
+            </form>
+        </section>
+    )
+}
+
+export default ContactForm;
